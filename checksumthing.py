52b0242ead82f3bf67755b97f462508eaa0e892f
Morgan Oscar Morel <morgan.morel@georgeblood.com>
2016-11-09 11:35:08 -0500

    working with cases

--- checksumthing.py
+++ checksumthing.py
@@ -14,24 +14,32 @@ def main():
 	parser.add_argument('-i','--inputDirectory',dest='i',help='The Directory To Process')
 	parser.add_argument('-t','--typeOfHash',dest='t',default='md5',help='Select the checksum type to process')
 	parser.add_argument('-ie','--inputExtension',dest='ie',default='.md5',help='Select the extension of the input files')
+	parser.add_argument('-c','--caseOutput',dest='c',default='lower',help='Define the case of the output hash letters. use lower or upper, defaults to lower')
+	
 	args = parser.parse_args()
 	
 	print "Starting analysis on " + args.t + " type checksums in " + os.path.basename(args.i)
 	
-	print args.ie
+	if args.t == 'md5':
+	    hashLength = 32
+	elif args.t == 'sha1':
+	    hashLength = 40
+	elif args.t == 'sha256':
+	    hashLength = 64
+	
+	
+	
 	os.chdir(args.i)
-	for filename in glob.glob('*' + args.ie, recursive=True):
-	    print filename
+	for filename in glob.glob('*' + args.ie):
+	    with open(filename, 'r') as f:
+	        text = f.read()
+	        hashValue = text[:hashLength]
+	        if args.c == 'lower':
+	            hashValue = hashValue.lower()
+	        if args.c == 'upper':
+	            hashValue = hashValue.upper()
+            print hashValue
 
 
-#        with open(filename, 'r') as f:
-            #text = f.read()
-            #checkSum = text.split('*')[0].strip()
-            #wavFile = text.split('*')[1].replace("\n", "")
-
-            #checkSum_dict[str(wavFile)] = {}
-            #checkSum_dict[str(wavFile)]['md5'] = checkSum
-            #checkSum_dict[str(wavFile)]['md5_datetime'] = timestr
-            #checkSum_dict[str(wavFile)]['filename'] = wavFile
 	
 main()
