3702c32beb3ccceb9ad8c5e30b670fc5db2b78f3
Henry Borchers <henryborchers@yahoo.com>
2016-11-10 13:36:11 -0500

    added docstrings

--- Checksumthing/manifest_formats/abs_manifestFormats.py
+++ Checksumthing/manifest_formats/abs_manifestFormats.py
@@ -5,4 +5,10 @@ class ABS_Manifest(object):
     __metaclass__ = abc.ABCMeta
     @abc.abstractmethod
     def write_to_manifest(self, manifest_path, hash_path, hash):
+        """Use this base class to implement different manifest reports
+
+        :param manifest_path: Path to the original file that the hash points to.
+        :param hash_path: The path to the sidecar file which contains the hash file.
+        :param hash: The hash value
+        """
         pass
--- Checksumthing/manifest_formats/manifest.py
+++ Checksumthing/manifest_formats/manifest.py
@@ -1,6 +1,17 @@
 class Manifest:
+    """This is for dynamically loading the various manifest formats. In the strategy pattern, this is the context
+    class.
+
+    """
     def __init__(self, manifestStyle):
+        """
+        Used to choose the correct strategy for writing out a manifest.
+
+        :param manifestStyle: Manifest pattern. This needs to implement the ABS_Manifest base class to conform.
+        :type manifestStyle: ABS_Manifest
+        """
         self._strategy = manifestStyle
 
     def add_line(self, manifest_path, hash_path, hash):
+
         self._strategy.write_to_manifest(manifest_path, hash_path, hash)
